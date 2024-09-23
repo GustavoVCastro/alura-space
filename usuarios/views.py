7d2abb492e1b97cfd31e15ba653962053629588b
Gustavo Castro <45861294+GustavoVCastro@users.noreply.github.com>
2024-09-23 17:34:35 -0300

    Login/signup/logout logic and messages

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,9 +2,34 @@ from django.shortcuts import redirect, render
 
 from usuarios.forms import LoginForms, CadastroForms
 from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
 
 def login(request):
     form = LoginForms()
+
+    if (request.method == 'POST'):
+        form = LoginForms(request.POST)
+
+        if (form.is_valid()):
+            nome = form["nome_login"].value()
+            senha = form["senha"].value()
+
+            usuario = auth.authenticate(
+                request,
+                username = nome,
+                password = senha,
+            )
+
+            if (usuario is not None):
+                auth.login(request, usuario)
+                messages.success(request, f"Usuário {nome} logado com sucesso!")
+
+                return redirect("index")
+            else:
+                messages.error(request, "Erro ao efetuar login.")
+                return redirect("login")
+
 
     return render(request, "usuarios/login.html", {"form": form})
 
@@ -17,15 +42,17 @@ def cadastro(request):
         if (form.is_valid()):
             # Validate password match
             if (form["senha_1"].value() != form["senha_2"].value()):
+                messages.error(request, "Senhas não são iguais.")
                 return redirect('cadastro')
 
-            # Get user atributes
+            # Get user attributes
             nome=form["nome_cadastro"].value()
             email=form["email"].value()
             senha=form["senha_1"].value()
 
             # Check for existing user
             if (User.objects.filter(username=nome).exists()):
+                messages.error(request, f"Usuário já existe")
                 return redirect('cadastro')
 
             # Add new user to db
@@ -35,6 +62,12 @@ def cadastro(request):
                  password = senha,
             )
             usuario.save()
+            messages.success(request, f"Cadastro efetuado com sucesso!")
             return redirect('login')
 
     return render(request, "usuarios/cadastro.html", {"form": form})
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "Logout efetuado com sucesso.")
+    return redirect('login')
