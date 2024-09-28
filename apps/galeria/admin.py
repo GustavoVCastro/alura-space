f3d9a76b5d47afa15f947843d21773ab06305e5c
Gustavo Castro <45861294+GustavoVCastro@users.noreply.github.com>
2024-09-28 18:42:34 -0300

    Templates refactored and new image CRUD for users

--- apps/galeria/admin.py
+++ apps/galeria/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from galeria.models import Fotografia
+from apps.galeria.models import Fotografia
 
 class ListandoFotografias(admin.ModelAdmin):
     list_display = ("id", "nome", "legenda", "publicada")
@@ -8,6 +8,6 @@ class ListandoFotografias(admin.ModelAdmin):
     search_fields = ("nome",)
     list_filter = ("categoria","usuario",)
     list_editable = ("publicada",)
-    list_per_page = 2
+    list_per_page = 10
 
 admin.site.register(Fotografia, ListandoFotografias)
