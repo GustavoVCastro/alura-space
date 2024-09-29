19687c5b794e93a6d12f78d745aba949bb8303da
Gustavo Castro <45861294+GustavoVCastro@users.noreply.github.com>
2024-09-29 18:31:00 -0300

    Edit and delete images. Filter.

--- apps/galeria/models.py
+++ apps/galeria/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 from datetime import datetime
 from django.contrib.auth.models import User
-from django.db.models.fields import related
 
 class Fotografia(models.Model):
 
